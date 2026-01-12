a5def3c6b7d2a94c781c3b01454172566c97a41a
vaishu <vaishu@gmail.com>
2026-01-12 23:12:01 +0530

    Fix module issue for Render

--- client/src/services/todoService.js
+++ client/src/services/todoService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/todos';
+const API_URL = 'https://to-do-tc9h.onrender.com/api/todos';
 
 const todoService = {
   getAllTodos: async () => {
